da6ae5e5651b266dd0ca6855b271dc4bb955e1e7
bibek <bibek.cn@20shots.co.uk>
2022-05-18 18:00:45 +0530

    added certbot

--- execute.sh
+++ execute.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 cd toolbox-docker-deploy
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 304646199800.dkr.ecr.eu-west-2.amazonaws.com
 docker-compose stop
 docker-compose rm -f
 docker-compose pull   
 docker-compose up -d
+
