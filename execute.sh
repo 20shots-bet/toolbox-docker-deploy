4f2c1862fdc119b6d28842420a7aae8f54c74b0f
bibek <bibek.cn@20shots.co.uk>
2022-05-18 19:11:25 +0530

    added sync

--- execute.sh
+++ execute.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 cd toolbox-docker-deploy
+git pull
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 304646199800.dkr.ecr.eu-west-2.amazonaws.com
 docker-compose stop
 docker-compose rm -f
